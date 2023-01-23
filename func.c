8fa906cb9e936241bf20ea2a1322c7ca766a8724
Nice22 <benothouessou74@gmail.com>
2023-01-23 09:01:03 -0800

    func

--- func.c
+++ func.c
@@ -17,11 +17,11 @@
  */
 
 int print_char(va_list types, char buffer[],
- int flags, int width, int precision, int size)
+int flags, int width, int precision, int size)
 {
- char c = va_arg(types, int);
+	char c = va_arg(types, int);
 
- return (handle_write_char(c, buffer, flags, width, precision, size));
+	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
 
 /**

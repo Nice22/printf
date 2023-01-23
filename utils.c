f2ed0cd286bf384614fe10d028c49f92d1928986
Nice22 <benothouessou74@gmail.com>
2023-01-23 15:37:19 -0800

    utils.c

--- utils.c
+++ utils.c
@@ -13,10 +13,9 @@
 
 int is_printable(char c)
 {
- if (c >= 32 && c < 127)
-  return (1);
-
- return (0);
+	if (c >= 32 && c < 127)
+		return (1);
+	return (0);
 }
 
 /**
@@ -28,18 +27,16 @@ int is_printable(char c)
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
- char map_to[] = "0123456789ABCDEF";
- /* The hexa format code is always 2 digits long */
- if (ascii_code < 0)
-  ascii_code *= -1;
+	char map_to[] = "0123456789ABCDEF";
 
- buffer[i++] = '\\';
- buffer[i++] = 'x';
+	if (ascii_code < 0)
+		ascii_code *= -1;
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
 
- buffer[i++] = map_to[ascii_code / 16];
- buffer[i] = map_to[ascii_code % 16];
-
- return (3);
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+	return (3);
 }
 
 /**
@@ -50,10 +47,9 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
  */
 int is_digit(char c)
 {
- if (c >= '0' && c <= '9')
-  return (1);
-
- return (0);
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
 }
 
 /**
@@ -65,12 +61,11 @@ int is_digit(char c)
  */
 long int convert_size_number(long int num, int size)
 {
- if (size == S_LONG)
-  return (num);
- else if (size == S_SHORT)
-  return ((short)num);
-
- return ((int)num);
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+	return ((int)num);
 }
 
 /**
@@ -82,10 +77,10 @@ long int convert_size_number(long int num, int size)
  */
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
- if (size == S_LONG)
-  return (num);
- else if (size == S_SHORT)
-  return ((unsigned short)num);
-
- return ((unsigned int)num);
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+	return ((unsigned int)num);
 }
+

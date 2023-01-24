a3721baa638cf71403c397dfd4b3c48bc37ba8a8
Nice22 <benothouessou74@gmail.com>
2023-01-24 18:27:39 +0100

    main.h

--- main.h
+++ main.h
@@ -107,5 +107,7 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
+void print_buffer(char buffer[], int *buff_ind);
+
 #endif
 
